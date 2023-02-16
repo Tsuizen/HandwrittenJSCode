066d407ace80dbcf7e48e78298411ee2c6fa663c
Tsuzien <personal_rogers@outlook.com>
2023-02-16 22:32:10 +0800

    perf: ⚡️ flat

--- 数组扁平化.js
+++ 数组扁平化.js
@@ -1,13 +1,25 @@
 let arr = [1, [2, [3, 4]]];
 
-function flatten(arr) {
-  while (arr.some(item => Array.isArray(item))) {
+function flatten(arr, deep) {
+  while (arr.some(item => Array.isArray(item)) && deep) {
     arr = [].concat(...arr);
+    deep--;
   }
   return arr;
 }
 
-console.log(flatten(arr)); //  [1, 2, 3, 4，5]
+Array.prototype.flatten = function (deep = 1) {
+  let temp = this;
+  while (temp.some(item => Array.isArray(item)) && deep) {
+    temp = [].concat(...temp);
+    deep--;
+  }
+  return temp;
+}
+
+console.log(arr.flatten(1));
+console.log(arr);
+// console.log(flatten(arr, 1)); //  [1, 2, 3, 4，5]
 
 // flat
-console.log(arr.flat(Infinity))
+// console.log(arr.flat(Infinity))
