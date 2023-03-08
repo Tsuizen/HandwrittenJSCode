d9ded8685071c187baa2e30a31fc9b3069d63362
Tsuzien <personal_rogers@outlook.com>
2023-03-09 00:49:36 +0800

    feat: 🎸 增加qwik中洗牌算法，其实思路差不多

--- 数组乱序输出.js
+++ 数组乱序输出.js
@@ -8,6 +8,19 @@ function shuffle(array) {
 
   return array;
 }
+
 let array = new Array(52).fill(0).map((_, idx) => idx + 1);
 
-console.log(shuffle(array));
+//qwik框架中的实现
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+console.log(shuffleArray(array));
+
+
+
