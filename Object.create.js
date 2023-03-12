8a0a29040774f85bf02f08c670f6162b5a572eb9
Tsuzien <personal_rogers@outlook.com>
2023-03-12 11:11:00 +0800

    feat: 🎸 验证Object.create

--- Object.create.js
+++ Object.create.js
@@ -1,3 +1,5 @@
+// 以传入的对象为原型创建一个新对象
+
 function create(obj) {
   function F() {
   }
@@ -13,6 +15,10 @@ const obj = {
   }
 }
 
+obj.name = 'Tsuizen';
+
 const copy = create(obj);
 copy.printIntroduction()
+
+console.log(Object.getPrototypeOf(copy))
 
